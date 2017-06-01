90bf5ce005eaeac472eb7d3b8270e88d48950998
Mike Dubman <miked@mellanox.com>
2017-06-01 14:35:43 +0300

    github pull request trigger

--- jobs/seed_all.groovy
+++ jobs/seed_all.groovy
@@ -34,18 +34,18 @@ job('Seed1 test') {
 
   scm {
     git {
-	    remote {
-		    github('miked-mellanox/devops','ssh')
-                    refspec('+refs/pull/*:refs/remotes/origin/pr/*')
-		    credentials('549927eb-7f38-4a8f-997a-81dd63605782')
-	    }
-	    branch('${sha1}')
+	remote {
+	        github('miked-mellanox/devops','ssh')
+                refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+	        credentials('549927eb-7f38-4a8f-997a-81dd63605782')
+	}
+	branch('${sha1}')
     }
   }
 
 
   triggers {
-      githubPullRequest {
+      pullRequest {
           admin('mellanox-hpc')
           admins(['Mellanox','hcoll-team'])
           orgWhitelist(['mellanox-hpc', 'Mellanox'])
